dfc3a63486031aa842ff7e1ac4b3d036e6626ea8
kashiomaru <shinka0831s@gmail.com>
2026-02-22 21:27:02 +0900

    成長倍率反映

--- Assets/Scripts/UpgradeData.cs
+++ Assets/Scripts/UpgradeData.cs
@@ -5,7 +5,6 @@ public struct UpgradeData
 {
     public UpgradeType type;
     public string title;
-    public Sprite icon; // アイコンがあれば
     /// <summary>出現確率の重み。0=出現しない。大きいほど出現しやすい。</summary>
     public int weight;
 }
